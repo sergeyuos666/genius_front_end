f4225e49bac70212bd5cf89c56be5096f95ade6e
sergey <98664691+sergeyuos666@users.noreply.github.com>
2023-11-05 13:11:31 +0200

    05.11

---
+++ work/js/main.js
@@ -0,0 +1,9 @@
+const modalWindows = document.querySelector('.backdrop');
+const modalBtnopen = document.querySelector('.header__btn');
+const modalBtnclose = document.querySelector('.modal__btn_close');
+
+const toggleModal = () => modalWindows.classList.toggle('is-hidden');
+
+modalBtnopen.addEventListener('click', toggleModal);
+
+modalBtnclose.addEventListener('click', toggleModal);
